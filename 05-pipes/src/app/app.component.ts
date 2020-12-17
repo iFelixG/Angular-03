f8bda821b8a85d5ca2cc5ec75340eb751180146d
Felix <diosfrag@gmail.com>
2020-12-17 01:55:47 -0600

    empezando seccion 6

---
+++ 05-pipes/src/app/app.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  nombre: string = 'Capitan America';
+  arreglo = [1,2,3,4,5,6,7,8,9,10];
+  PI: number = Math.PI;
+  porcentaje: number = 0.234;
+  salario: number = 123.5;
+
+  heroe = {
+  	nombre: 'Logan',
+  	clave: 'Wolverine',
+  	edad: 100,
+  	direccion: {
+  		calle: 'Primera',
+  		casa: 20
+  	}
+  }
+}
